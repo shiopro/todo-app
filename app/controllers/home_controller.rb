11fae8e236b429b95d8578b662bac532668c165d
長野汐央里 <>
2024-07-20 21:26:00 +0900

    ユーザが複数のボードを作成できるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,25 @@
 class HomeController < ApplicationController
   def index
-
   end
 
   def new
-    @board = Board.new
+    @board = current_user.boards.build
+  end
+
+  def show
   end
 
   def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to home_path(@board)
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :description)
   end
 end
