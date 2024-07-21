a470df7d1a3399f178514b7581cd2b0139ba0b4e
長野汐央里 <>
2024-07-21 17:11:33 +0900

    flashメッセージの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,8 +12,9 @@ class HomeController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to home_path(@board)
+      redirect_to home_path(@board), notice: '保存できました'
     else
+      flash.now[:error] = '保存に失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
