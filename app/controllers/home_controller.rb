60eee3be9dc1f65c0f5ba88fc54e9fa52776d0b8
長野汐央里 <>
2024-07-28 17:51:27 +0900

    ボードの削除をできるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to home_path(@board), notice: '保存できました'
+      redirect_to root_path, notice: '保存できました'
     else
       flash.now[:error] = '保存に失敗しました'
       render :new, status: :unprocessable_entity
@@ -32,6 +32,12 @@ class HomeController < ApplicationController
       flash.now[:error] = '更新できませんでした'
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    board = current_user.boards.find(params[:id])
+    board.destroy!
+    redirect_to root_path, notice: '削除に成功しました'
   end
 
   private
