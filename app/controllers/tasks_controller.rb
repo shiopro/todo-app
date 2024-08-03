e520ad749df9bdc8797c5e6d977e04b5a569b95c
長野汐央里 <>
2024-08-04 00:04:26 +0900

    taskにタイトル、内容、期限が登録できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,27 @@
 class TasksController < ApplicationController
+
+  
+
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
   root to: 'home#index'
+
   resources :home, only: %i[show new create edit update destroy]
+  resources :tasks, only: [:index, :new, :create]
   # Defines the root path route ("/")
   # root "posts#index"
 end
